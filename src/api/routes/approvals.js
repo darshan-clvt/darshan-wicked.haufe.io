9b91a9c7320a0f67dc35bd07b1400b993ad4547a
darshan-clvt <darshan.g@clarivate.com>
2023-08-11 16:43:36 +0530

    decline approvals by DateRange [snapshot]

--- src/api/routes/approvals.js
+++ src/api/routes/approvals.js
@@ -7,13 +7,17 @@ const utils = require('./utils');
 const users = require('./users');
 
 const dao = require('../dao/dao');
-
+const daoUtils = require('../dao/dao-utils');
+const webhooks = require('./webhooks');
+const subscription =require('./subscriptions');
 const approvals = require('express').Router();
 
 // ===== SCOPES =====
 
 const READ_SCOPE = 'read_approvals';
+const WRITE_SCOPE = 'write_subscriptions';
 const verifyReadScope = utils.verifyScope(READ_SCOPE);
+const verifyWriteScope =utils.verifyScope(WRITE_SCOPE);
 
 // ===== ENDPOINTS =====
 
@@ -23,6 +27,18 @@ approvals.get('/', verifyReadScope, function (req, res, next) {
 
 approvals.get('/:approvalId', verifyReadScope, function (req, res, next) {
     approvals.getApproval(req, res, next, req.apiUserId, req.params.approvalId);
+});
+//get By DateRange routes
+approvals.get('/byDates/:startDate/:endDate', verifyReadScope, function (req, res, next) {
+    const startDate = req.params.startDate;
+    const endDate = req.params.endDate;
+    approvals.getApprovalByDates(req, res, next, req.apiUserId, startDate, endDate);
+});
+//Delete By DateRange routes
+approvals.delete('/byDates/:startDate/:endDate',verifyWriteScope, function (req, res, next){
+    const startDate = req.params.startDate;
+    const endDate = req.params.endDate;
+    approvals.deleteApprovalsByDates(req, res, next, req.apiUserId, startDate, endDate);
 });
 
 // ===== IMPLEMENTATION =====
@@ -60,6 +76,43 @@ function getAllApprovals(app, loggedInUserId, callback) {
         loadAllApprovals(app, userInfo, callback);
     });
 }
+approvals.getApprovalByDates = function (req, res, next, loggedInUserId, startDate, endDate) {
+    getAllApprovals(req.app, loggedInUserId, (err, approvalInfos) => {
+        if (err) {
+            return utils.failError(res, err);
+        }
+        const decodedStartDate = decodeURIComponent(startDate);
+        const decodedEndDate = decodeURIComponent(endDate);
+        const approvalInfo = approvalInfos.filter(a => a.changedDate >= decodedStartDate && a.changedDate <= decodedEndDate);
+        if (!approvalInfo) {
+            return utils.fail(res, 404, 'Not found');
+        }
+        return res.json(approvalInfo);
+    });
+};
+
+approvals.deleteApprovalsByDates = function (req, res, next, loggedInUserId, startDate, endDate) {
+    getAllApprovals(req.app, loggedInUserId, (err, approvalInfos) => {
+        if (err) {
+            return utils.failError(res, err);
+        }
+        const decodedStartDate = decodeURIComponent(startDate);
+        const decodedEndDate = decodeURIComponent(endDate);
+
+        const approvalInfo = approvalInfos.filter(a => a.changedDate >= decodedStartDate && a.changedDate <= decodedEndDate);
+        if (approvalInfo.length === 0) {
+            return utils.fail(res, 404, 'Not found');
+        }
+        for (let approval of approvalInfo){
+            dao.subscriptions.delete(approval.application.id, approval.api.id, approval.subscriptionId, (err) => {
+                      if (err) {
+                          return utils.fail(res, 500, 'deleteSubscription: DAO delete subscription failed',err);
+                      }
+                       res.status(204).send('');
+                  });
+              };
+    });
+};
 
 function checkRights(app, loggedInUserId, callback) {
     if (!loggedInUserId) {
