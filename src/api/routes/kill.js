541275e7286653e936d8201546b22d50350cf2c0
Sai Gaddam <saicharan.gaddam@Clarivate.com>
2024-04-03 22:14:24 +0530

    adding checks in restart

--- src/api/routes/kill.js
+++ src/api/routes/kill.js
@@ -18,10 +18,11 @@ if (fs.existsSync(KILL_SEMAPHORE)) {
 }
 
 const verifyKillScope = utils.verifyScope('restart_api');
+const isRestartAllowed = utils.checkRestartKey()
 
 // ===== ENDPOINTS =====
 
- kill.post('/', verifyKillScope, function (req, res, next) {
+ kill.post('/', isRestartAllowed, function (req, res, next) {
      debug('POST /kill');
      kill.killApi(req.app, res, req.apiUserId);
     });
@@ -29,24 +30,13 @@ const verifyKillScope = utils.verifyScope('restart_api');
 // ===== IMPLEMENTATION =====
 
 kill.killApi = function (app, res, loggedInUserId) {
-    debug('killApi()');
-    users.loadUser(app, loggedInUserId, (err, userInfo) => {
-        if (err) {
-            return utils.fail(res, 500, 'getApplications: Could not load user.', err);
-        }
-        if (!userInfo) {
-            return utils.fail(res, 403, 'Not allowed.');
-        }
-        if (!userInfo.admin && !userInfo.approver) {
-            return utils.fail(res, 403, 'Not allowed. This is admin land.');
-        }
+        debug('killApi()');
         warn('RESTARTING API DUE TO USER REQUEST');
         fs.writeFileSync(KILL_SEMAPHORE, 'reload requested by API interaction.', 'utf8');
         res.status(204).json({});
         setTimeout(function () {
             process.exit(0);
         }, 1000);
-    });
 };
 
 module.exports = kill;
